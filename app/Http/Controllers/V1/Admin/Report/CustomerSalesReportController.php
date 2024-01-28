c2267dd4d35d86368e72087e2961d8d04e396d29
John Adriaans <adriaanscorne@gmail.com>
2024-01-28 18:28:17 +0200

    added csv generator for customer sales report

--- app/Http/Controllers/V1/Admin/Report/CustomerSalesReportController.php
+++ app/Http/Controllers/V1/Admin/Report/CustomerSalesReportController.php
@@ -27,19 +27,21 @@ class CustomerSalesReportController extends Controller
 
         $this->authorize('view report', $company);
 
-        $locale = CompanySetting::getSetting('language',  $company->id);
+        $locale = CompanySetting::getSetting('language', $company->id);
 
         App::setLocale($locale);
 
         $start = Carbon::createFromFormat('Y-m-d', $request->from_date);
         $end = Carbon::createFromFormat('Y-m-d', $request->to_date);
 
-        $customers = Customer::with(['invoices' => function ($query) use ($start, $end) {
-            $query->whereBetween(
-                'invoice_date',
-                [$start->format('Y-m-d'), $end->format('Y-m-d')]
-            );
-        }])
+        $customers = Customer::with([
+            'invoices' => function ($query) use ($start, $end) {
+                $query->whereBetween(
+                    'invoice_date',
+                    [$start->format('Y-m-d'), $end->format('Y-m-d')]
+                );
+            }
+        ])
             ->where('company_id', $company->id)
             ->applyInvoiceFilters($request->only(['from_date', 'to_date']))
             ->get();
@@ -58,6 +60,11 @@ class CustomerSalesReportController extends Controller
         $from_date = Carbon::createFromFormat('Y-m-d', $request->from_date)->format($dateFormat);
         $to_date = Carbon::createFromFormat('Y-m-d', $request->to_date)->format($dateFormat);
         $currency = Currency::findOrFail(CompanySetting::getSetting('currency', $company->id));
+
+        // download csv
+        if ($request->has('download') && $request->has("csv")) {
+            return $this->downloadCSV($customers, $from_date, $to_date, $totalAmount, $currency, $company);
+        }
 
         $colors = [
             'primary_text_color',
@@ -96,5 +103,52 @@ class CustomerSalesReportController extends Controller
         }
 
         return $pdf->stream();
+    }
+
+    /**
+     * Creates CSV file for download
+     * 
+     * @param array[] $customers
+     * @param string $from_date
+     * @param string $to_date
+     * @param float $totalAmount
+     * @param Currency $currency
+     * @param Company $company
+     * @return \Illuminate\Support\Facades\Response
+     */
+    private function downloadCSV($customers, $from_date, $to_date, $totalAmount, $currency, $company)
+    {
+        $csvFileName = 'salesByCustomer.csv';
+        $headers = [
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachment; filename="' . $csvFileName . '"',
+        ];
+
+        $handle = fopen('php://output', 'w');
+        fputcsv($handle, [$company->name, '', '', '']);
+        fputcsv($handle, ['', '', '', '']);
+        fputcsv($handle, [trans('pdf_customer_sales_report'), $from_date . ' - ' . $to_date]);
+        fputcsv($handle, ['Customer', 'Date', 'Invoice Number', 'Amount']);
+
+        foreach ($customers as $customer) {
+            $index = 0;
+            foreach ($customer->invoices as $invoice) {
+                $row = [$invoice->formattedInvoiceDate, $invoice->invoice_number, format_money($invoice->base_total, $currency)];
+                if ($index == 0) {
+                    array_unshift($row, $customer->name);
+                } else {
+                    array_unshift($row, '');
+                }
+                fputcsv($handle, $row);
+                $index++;
+            }
+        }
+
+        fputcsv($handle, ['----', '----', '----', '----']);
+        fputcsv($handle, ['Total', '', '', format_money($totalAmount, $currency)]);
+
+        fclose($handle);
+
+        return Response::make('', 200, $headers);
     }
 }
